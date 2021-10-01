6e5b413f273f5eae811bbdf773b0e877adf5a20c
BrandonGPace <brandon.gary.pace@aggiemail.usu.edu>
2021-10-01 13:36:06 -0600

    pmove
    added comments

--- pMove.py
+++ pMove.py
@@ -1,37 +1,49 @@
-from tau import tau
+# Description
+'''
+The purpose of pMove is to calculate the odds of the EC traveling to each space around it
+'''
 
 
-def pMove(x, y, direction, pro, fib, vegf, xSteps, ySteps, lamda, k):
+# Imports
+# File Import
+from Tau import tau
+
+
+# Function
+def pMove(x, y, direction, protease, fibronectin, vegf, xSteps, ySteps, lam, k):
+
+    # T is the chance that it will go left, right, up, and down
     T = [0, 0, 0, 0]
 
-    # LEFT
+    # Left
     if x > 0:
-        T[0] = tau(pro[2*y][x-1], fib[2*y][x-1], vegf[2*y][x-1], y)
+        T[0] = tau(protease[2*y][x-1], fibronectin[2*y][x-1], vegf[2*y][x-1], y)
     else:
         T[0] = 0
 
-    # RIGHT
+    # Right
+    # Don't need to add 1 to x because the x substrate should be offset by a half step from the cell matrix meshpoints
     if x < xSteps-1:
-        T[1] = tau(pro[2*y][x], fib[2*y][x], vegf[2*y][x], y)       # don't need to add 1 to x because the x substrate should be offset by a half step from the cell matrix meshpoints
+        T[1] = tau(protease[2*y][x], fibronectin[2*y][x], vegf[2*y][x], y)
     else:
         T[1] = 0
 
-    # UP
+    # Up
     if y > 1:
-        T[2] = tau(pro[2*y-1][x], fib[2*y-1][x], vegf[2*y-1][x], y)
+        T[2] = tau(protease[2*y-1][x], fibronectin[2*y-1][x], vegf[2*y-1][x], y)
     else:
         T[2] = 0
 
-    # DOWN
+    # Down
     if 0 < y < ySteps - 1:
-        T[3] = tau(pro[2*y+1][x], fib[2*y+1][x], vegf[2*y+1][x], y)
+        T[3] = tau(protease[2*y+1][x], fibronectin[2*y+1][x], vegf[2*y+1][x], y)
     else:
         T[3] = 0
 
-    # probability equations from page 153 and 152
+    # Probability equations from page 153 and 152 of the plank paper
     if y == 0:
-        pmove = 2 * lamda * k * T[direction] / (T[0]+T[1])
+        pmove = 2 * lam * k * T[direction] / (T[0]+T[1])
     else:
-        pmove = 4 * lamda * k * T[direction] / (T[0]+T[1]+T[2]+T[3])
+        pmove = 4 * lam * k * T[direction] / (T[0]+T[1]+T[2]+T[3])
 
     return pmove, T
