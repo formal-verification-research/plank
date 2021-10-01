f4b4fef83ee956a8ac26512947ba8e6ff863c45b
BrandonGPace <brandon.gary.pace@aggiemail.usu.edu>
2021-10-01 13:37:22 -0600

    pStay
    added comments

--- pStay.py
+++ pStay.py
@@ -1,8 +1,16 @@
-def pStay(y, lamda, k):
-    # probability equations found on page 152 and 153
+# Description
+'''
+The purpose of pStay is to determine the chances that the cell will not more for the current time step
+'''
+
+
+# Function
+def pStay(y, lam, k):
+
+    # Probability equations found on page 152 and 153 of the plank paper
     if y == 0:
-        pstay = 1 - 2 * lamda * k
+        pstay = 1 - 2 * lam * k
     else:
-        pstay = 1 - 4 * lamda * k
+        pstay = 1 - 4 * lam * k
 
     return pstay
